ae418477b3e10dd5765b5d280729ac6bd4742b9b
Waldemar Adams <waldek-adamski@wp.pl>
2020-09-14 22:25:57 +0200

    stop fn 'timer'if seconds <0

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,9 +1,16 @@
+let countdown;
+
 function timer (seconds){
     const now = Date.now();
     const then = now + seconds * 1000;
 
-    setInterval(()=>{
+    countdown = setInterval(()=>{
         const secondsLeft = Math.round((then - Date.now()) / 1000);
         console.log(secondsLeft);
+         if(secondsLeft<0){
+             clearInterval(countdown);
+             return;
+         }
+
     },1000)
 }
