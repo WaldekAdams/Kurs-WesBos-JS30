62a0064b71d7691fc4092bc153a852de5a2f4f15
Waldemar Adams <waldek-adamski@wp.pl>
2020-09-15 07:52:56 +0200

    display seconds remaining in div

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,23 +1,29 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
 
-function timer (seconds){
+function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
 
-    countdown = setInterval(()=>{
+    countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
-        if(secondsLeft<0){
+        if (secondsLeft < 0) {
             clearInterval(countdown);
             return;
         }
         displayTimeLeft(secondsLeft);
+        console.log('ok')
 
-    },1000)
+    }, 1000)
 }
-function displayTimeLeft(seconds){
-    console.log(seconds);
-    const minutes = Math.floor(seconds/60);
-    const remeinderSeconds = minutes % 60;
+
+function displayTimeLeft(seconds) {
+    // console.log(seconds);
+    const minutes = Math.floor(seconds / 60);
+    const remeinderSeconds = seconds % 60;
+    const display = `${minutes}:${remeinderSeconds < 10 ? '0':''} ${remeinderSeconds}`;
+    timerDisplay.textContent = display;
+    document.title = display;
 
 }
