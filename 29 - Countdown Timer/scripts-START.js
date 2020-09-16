ea4489df40f7d714dc7ca9b653ca61e830c68b68
Waldemar Adams <waldek-adamski@wp.pl>
2020-09-16 07:37:42 +0200

    display what time the counter ends

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,10 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time')
 
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
+    displayEndTime(then);
 
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -22,8 +24,14 @@ function displayTimeLeft(seconds) {
     // console.log(seconds);
     const minutes = Math.floor(seconds / 60);
     const remeinderSeconds = seconds % 60;
-    const display = `${minutes}:${remeinderSeconds < 10 ? '0':''} ${remeinderSeconds}`;
+    const display = `${minutes}:${remeinderSeconds < 10 ? '0':''}${remeinderSeconds}`;
     timerDisplay.textContent = display;
     document.title = display;
+}
 
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${hour}:${minutes}`
 }
