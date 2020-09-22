5f27a0b5cbb197883bca3f9170345d8607fb7fe0
Waldemar Adams <waldek-adamski@wp.pl>
2020-09-22 21:18:23 +0200

    get elements to variables

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,7 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toogle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
